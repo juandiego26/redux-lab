4962ad20f4385cff825015dec6de1ea2fe9faf2f
Juan Diego <silgajuandiego@gmail.com>
2018-10-14 09:39:12 -0500

    [Add] Clase No.9 definiendo las acciones e imprimiendo el estado

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,10 +1,46 @@
-// console.log('dame el trabajo porfa')
+import { createStore } from 'redux'
 
 const $form = document.getElementById('form')
 
-$form.addEventListener('submit', handleSubmit = (event) => {
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit (event) {
   event.preventDefault()
   const data = new FormData($form)
   const title = data.get('title')
   console.log(title)
+  store.dispatch({
+    type: 'ADD_SONG',
+    payload: {
+      title
+    }
+  })
+}
+
+const initialState = [
+  {
+    "title": "Despacito"
+  },
+  {
+    "title": "One more time"
+  },
+  {
+    "title": "Echame la culpa"
+  }
+]
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const $container = document.getElementById('playlist')
+const playlist = store.getState()
+playlist.forEach(item => {
+  const template = document.createElement('p')
+  template.textContent = item.title
+  $container.appendChild(template)
 })
+
+console.log(store.getState())
