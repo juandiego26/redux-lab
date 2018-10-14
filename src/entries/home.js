506a4f93ad5a79fcace776603f3e913ba5cf61dd
Juan Diego <silgajuandiego@gmail.com>
2018-10-14 15:45:43 -0500

    [Add] Clase No.15 Integrando el Store con el Provider de React-redux

--- src/entries/home.js
+++ src/entries/home.js
@@ -4,19 +4,32 @@ import Home from '../pages/containers/home'
 // import Playlist from '../playlist/components/playlist'
 import data from '../data/api.json'
 import extras	from '../data/api-extra.json'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+// estado inicial de los datos del api y se define el modelo como se va a consumir
+  const initialState = {
+    data: {
+      ...data
+    }
+  }
+// creamos nuestro store
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-
-//console.log('Hola Mundo!')
+console.log(store.getState())
 
 //Referencia por id en el index.html
 const homeContainer = document.getElementById('home-container');
 // Insertamos en una constante lo que queremos renderizar
 // const holaMundo =  <h1>Hola estudiantes</h1>
 // ReactDOM.render(que voy a renderizar(elemento h1,div,etc ó un componente), donde renderizar en el index html)
+
 render  (
-  <Home
-    data={data}
-    extras={extras}
-  />,
+  <Provider store={store}>
+    <p>Hola mundo</p>
+  </Provider>,
     homeContainer
 );
