b8ed81885dd9d7fbbd635a92f1f9d34f0b59680a
Juan Diego <silgajuandiego@gmail.com>
2018-10-14 12:00:40 -0500

    [Add] Clase No.11 Actualizando la aplicación en cada cambio con Subscribe

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,14 +1,15 @@
 import { createStore } from 'redux'
-
+// referenciamos el elemento
 const $form = document.getElementById('form')
-
+// escuchamos el evento
 $form.addEventListener('submit', handleSubmit)
-
+// función que escuha el evento para luego imprimirlo en pantalla
 function handleSubmit (event) {
   event.preventDefault()
   const data = new FormData($form)
   const title = data.get('title')
   console.log(title)
+  // despachar la acción para actualizarla
   store.dispatch({
     type: 'ADD_SONG',
     payload: {
@@ -16,7 +17,7 @@ function handleSubmit (event) {
     }
   })
 }
-
+// estado inicial
 const initialState = [
   {
     "title": "Despacito"
@@ -38,19 +39,31 @@ const reducer = function(state, action) {
       return state
   }
 }
-
+// creamos el store
 const store = createStore(
   reducer, // reemplzamos esto por un reducer
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-const $container = document.getElementById('playlist')
-const playlist = store.getState()
-playlist.forEach(item => {
-  const template = document.createElement('p')
-  template.textContent = item.title
-  $container.appendChild(template)
-})
+function render() {
+  // para imprimir la playlist en pantalla
+  const $container = document.getElementById('playlist')
+  const playlist = store.getState()
+  $container.innerHTML = ''
+  playlist.forEach(item => {
+    const template = document.createElement('p')
+    template.textContent = item.title
+    $container.appendChild(template)
+  })
+}
 
-console.log(store.getState())
+render()
+
+function handleChange() {
+  render()
+}
+
+store.subscribe(handleChange)
+
+// console.log(store.getState())
