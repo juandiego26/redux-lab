43621dbd8d0597358a6997d5df41baf0c1bab7d8
Juan Diego <silgajuandiego@gmail.com>
2018-10-15 14:37:53 -0500

    [Add] Clase No.19 Reto añadiendo todo el arbol para filtrar la busqueda

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,10 +1,16 @@
 function data( state, action ){
   switch ( action.type ) {
     case 'SEARCH_VIDEO': {
-      // action.payload.query
-      const list = state.data.categories[2].playlist
-      const results = list.filter((item) => {
-        return item.author.includes(action.payload.query)
+      // // action.payload.query
+      // const list = state.data.categories[2].playlist
+      // const results = list.filter((item) => {
+      //   return item.author.includes(action.payload.query)
+      // })
+      const results = []
+      state.data.categories.map((category) => {
+        return category.playlist.filter((item) => {
+          return item.author.toLowerCase().includes(action.payload.query.toLowerCase()) && results.push(item)
+        })
       })
         return {
           ...state,
