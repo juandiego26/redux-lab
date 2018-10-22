9b8487576e8b2074818fca123341bba46c28e87e
Juan Diego <silgajuandiego@gmail.com>
2018-10-22 09:22:46 -0500

    [Add] Clase No.32 Middlewares y Añadiendo múltiples middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,38 +1,46 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Home from '../pages/containers/home'
-// import data from '../data/api.json'
-// import extras	from '../data/api-extra.json'
-// redux
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from '../reducers/index'
-// import data from '../schemas/index'
 import { Map as map } from 'immutable' // --- se le coloca un alias en minusculas para verlo como funcion no como clase
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-// console.log(normalizedData)
-// console.log(data)
+// //--middleware ES5 --
+// function logger({ getState, dispatch }) {
+//   return (next) => { // next siguiente middleware
+//     return (action) => { // pasamos el action
+//       console.log('Este es mi nuevo estado', getState().toJS())
+//       console.log('vamos a enviar esta accción', action)
+//       const value = next(action)
+//       console.log('Este es mi nuevo estado', getState().toJS())
+//       return value
+//     }
+//   }
+// }
 
-// estado inicial de los datos del api y se define el modelo como se va a consumir
-  // const initialState = {
-  //   data: {
-  //     // ...data
-  //     entities: data.entities,
-  //     categories: data.result.categories,
-  //     search: [],
-  //   },
-  //   extras,
-  //   modal: {
-  //     visibility: false,
-  //     mediaId: null
-  //   }
-  // }
+//-- Middleware ES6--
+
+const _logger = ({ getState, dispatch }) => next => action => {
+  console.log('Este es mi nuevo estado', getState().toJS())
+  console.log('vamos a enviar esta accción', action)
+  const value = next(action)
+  console.log('Este es mi nuevo estado', getState().toJS())
+  return value
+}
 
 // creamos nuestro store
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(
+      logger,
+      _logger
+    )
+  )
 )
 
 // console.log(store.getState())
